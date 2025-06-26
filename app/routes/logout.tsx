509bbcdc3e6116a5e8a12389992b93af8c1a3e5d
Sawera-Malik <attachannar8@gmail.com>
2025-06-26 14:14:54 +0500

    logout.tsx

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,7 +1,6 @@
 import { LoaderFunction, redirect } from "@remix-run/node";
 import { destroySession, getSession } from "../routes/session.server";
 
-// Helper function
 async function logoutAndRedirect(request: Request) {
     const session = await getSession(request.headers.get("Cookie"));
     return redirect("/login", {
@@ -11,7 +10,6 @@ async function logoutAndRedirect(request: Request) {
     });
 }
 
-// Remix loader
 export const loader: LoaderFunction = async ({ request }) => {
     return logoutAndRedirect(request);
 };
