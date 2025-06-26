b5119a2264013c6450a849fd75007c5bc31d29ba
Sawera-Malik <attachannar8@gmail.com>
2025-06-26 14:13:16 +0500

    Add files via upload

---
+++ app/routes/_index.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-800 p-4 " style={{ padding: 30, fontFamily: "sans-serif" }}>
+      <h1 className="text-4xl font-bold mb-5">Home</h1>
+      <a href="/login" target="_blank" className="text-blue-500 underline"> Go to Login Page</a>
+      <a href="/login" target="_blank" className="text-blue-500 underline"> Go to Card Page</a>
+
+    </div>
+  );
+}
